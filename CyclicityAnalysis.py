30390a52f044ff63714f23062457444f57e55366
vskaush2 <vskaush2@illinois.edu>
2022-03-23 11:14:28 -0500

    New Plotting features and capabilities in Component Phase Diagram and Lead Lag Matrix; New output of cyclic order as indices; fixed incorrect logic in computing the accumulated oriented area; fixed incorrect logic in computing top 10 leader follower pairs in CyclicityAnalysis.py

--- CyclicityAnalysis.py
+++ CyclicityAnalysis.py
@@ -16,7 +16,14 @@ def get_accumulated_oriented_area_df(df, col1, col2):
     accumulated_oriented_area_df = pd.DataFrame(columns=['Accumulated Oriented Area'], index=df.index)
     df_diff = df[[col1, col2]].diff().dropna()
     try:
-        first_oriented_area_change_time = df_diff[(df_diff[col1] != 0) & (df_diff[col2] != 0)].index[0]
+        df_index_dict = dict(enumerate(df.index))
+        col1_first_change_time = df_diff[(df_diff[col1] != 0)].index[0]
+        col2_first_change_time = df_diff[(df_diff[col1] != 0)].index[0]
+        first_oriented_area_change_time=list(sorted([col1_first_change_time, col2_first_change_time], key =lambda t: df_index_dict[t]))[-1]
+        # First time oriented area changes from 0 is the latest time when either x_{t}-x_{t-1} first becomes nonzero or y_{t}-y_{t-1} first becomes nonzero.
+
+
+
         oriented_area_change_times = df_diff[(df_diff[col1] != 0) | (df_diff[col2] != 0)].loc[first_oriented_area_change_time:].index
         accumulated_oriented_area_df['Accumulated Oriented Area'].loc[oriented_area_change_times] = [compute_oriented_area(df.loc[:time], col1, col2)
                                                                                                  for time in oriented_area_change_times]
@@ -69,7 +76,7 @@ class CyclicityAnalysis:
     def __init__(self,df):
         self.df = df
         self.lead_lag_df = self.get_lead_lag_df()
-        self.sorted_lead_lag_df, self.cyclic_order, self.sorted_eigvals, self.sorted_dominant_eigvec_components= self.get_cyclic_order()
+        self.sorted_lead_lag_df,  self.cyclic_order, self.cyclic_order_indices, self.sorted_eigvals, self.sorted_dominant_eigvec_components= self.get_cyclic_order()
 
     def get_lead_lag_df(self):
         unique_distinct_pairs = itertools.combinations(self.df.columns, r=2)
@@ -81,13 +88,22 @@ class CyclicityAnalysis:
         lead_lag_df.fillna(0, inplace=True)
         return lead_lag_df
 
-    def plot_lead_lag_df(self, color_label='Oriented Area', color_continuous_scale='Bluered'):
-        fig = px.imshow(self.sorted_lead_lag_df,
+    def plot_lead_lag_df(self, color_label='Oriented Area', color_continuous_scale='Bluered',sort_columns_by_cyclic_order=True):
+
+        fig = px.imshow(self.lead_lag_df,
                         labels = dict(color=color_label),
-                        x= self.sorted_lead_lag_df.columns,
-                        y =self.sorted_lead_lag_df.columns,
+                        x= self.lead_lag_df.columns,
+                        y =self.lead_lag_df.columns,
                         color_continuous_scale = color_continuous_scale)
+
+        if sort_columns_by_cyclic_order:
+            fig = px.imshow(self.sorted_lead_lag_df,
+                            labels = dict(color=color_label),
+                            x= self.sorted_lead_lag_df.columns,
+                            y =self.sorted_lead_lag_df.columns,
+                            color_continuous_scale = color_continuous_scale)
         fig.update_xaxes(side='top')
+
         return fig
 
     def get_cyclic_order(self):
@@ -113,9 +129,9 @@ class CyclicityAnalysis:
 
         cyclic_order = [self.lead_lag_df.columns[index] for index in cyclic_order_indices]
         sorted_lead_lag_df = self.lead_lag_df[cyclic_order].T[cyclic_order].T # Resorting Lead-Lag Matrix by Cyclic Order
-        return sorted_lead_lag_df, cyclic_order, sorted_eigvals, sorted_dominant_eigvec_components
+        return sorted_lead_lag_df, cyclic_order, cyclic_order_indices, sorted_eigvals, sorted_dominant_eigvec_components
 
-    def plot_eigenvalue_moduli_and_sorted_dominant_eigenvector_components(self,figsize=(20,10)):
+    def plot_eigenvalue_moduli_and_sorted_dominant_eigenvector_components(self, include_cyclic_order_arrows=False, use_cyclic_order_indices=False, shift=0.01, figsize=(20,10)):
         fig, axs = plt.subplots(1, 2, figsize=figsize)
         moduli = [np.absolute(eigval) for eigval in self.sorted_eigvals]
         axs[0].set_title("Moduli of Lead Lag Matrix Eigenvalues Plot", size=20)
@@ -128,26 +144,47 @@ class CyclicityAnalysis:
         axs[-1].set_ylabel('Im(z)', size=10)
         axs[-1].scatter(np.real(self.sorted_dominant_eigvec_components), np.imag(self.sorted_dominant_eigvec_components), s=50)
 
-        x_bound, y_bound =np.abs(np.max(np.real(self.sorted_dominant_eigvec_components))), \
-                          np.abs(np.max(np.imag(self.sorted_dominant_eigvec_components)))
+        x_bound, y_bound =np.max(np.abs(np.real(self.sorted_dominant_eigvec_components))), \
+                          np.max(np.abs(np.imag(self.sorted_dominant_eigvec_components)))
         # Largest of the real imaginary parts of eigenvector components in absolute value
 
-        axs[-1].set_xlim(-x_bound,x_bound)
-        axs[-1].set_ylim(-y_bound, y_bound)
-        axs[-1].hlines(0, -1, 1, color='black')
-        axs[-1].vlines(0, -1, 1, color='black')
+        axs[-1].set_xlim(-x_bound-shift,x_bound+shift)
+        axs[-1].set_ylim(-y_bound-shift, y_bound+shift)
+        axs[-1].hlines(0, -x_bound-shift, x_bound+shift, color='black')
+        axs[-1].vlines(0, -y_bound-shift, y_bound+shift, color='black')
+
         for i in range(len(self.sorted_dominant_eigvec_components)):
             component=(np.real(self.sorted_dominant_eigvec_components[i]), np.imag(self.sorted_dominant_eigvec_components[i]))
-            axs[-1].annotate(list(reversed(self.cyclic_order))[i], component , size=20) # Plotting Principal Dominant Eigenvector Components
+            if use_cyclic_order_indices:
+                axs[-1].annotate(list(reversed(self.cyclic_order_indices))[i], component , size=20) # Labeling Cyclic Order as Indices
+            else:
+                axs[-1].annotate(list(reversed(self.cyclic_order))[i], component , size=20) # Labeling Cyclic Order as original feature names
+
+        if include_cyclic_order_arrows:
+            for i in range(len(self.sorted_dominant_eigvec_components) - 1):
+                tail_x, tail_y = np.real(self.sorted_dominant_eigvec_components[i]), np.imag(
+                    self.sorted_dominant_eigvec_components[i])
+                head_x, head_y = np.real(self.sorted_dominant_eigvec_components[i + 1]), np.imag(
+                    self.sorted_dominant_eigvec_components[i + 1])
+
+                axs[-1].annotate("", xy=(head_x, head_y), xytext=(tail_x, tail_y),
+                                 arrowprops=dict(arrowstyle="->"), size=60)
+
+
 
 
     def get_topN_leader_follower_pairs(self,N=10):
         all_pairs = itertools.product(self.lead_lag_df.columns, repeat=2)
         oriented_areas = self.lead_lag_df.values.flatten()
         oriented_areas_dict = dict(zip(all_pairs,oriented_areas))
-        oriented_areas_dict = dict(sorted(oriented_areas_dict.items(),key=lambda x: x[-1],reverse=True))
-        topN_leader_follower_pairs = list(oriented_areas_dict.keys())[:N]
-        return topN_leader_follower_pairs
+        oriented_areas_dict = dict([item for item in oriented_areas_dict.items() if item[-1]>0]) # Only consider positive oriented areas
+
+        try:
+            oriented_areas_dict = dict(sorted(oriented_areas_dict.items(),key=lambda item: item[-1],reverse=True))
+            topN_leader_follower_pairs = oriented_areas_dict[:N]
+            return topN_leader_follower_pairs
+        except:
+            return []
 
 
 
